cf4a5af6075da3fc0bcef2486c1f30b81a8dd8f2
Ahmed Adjibade <ahmedadjibade@MacBook-Pro-de-Ahmed-2.local>
2021-11-16 15:30:34 +0100

    gale ok but one config more

--- _build/default/src/mariages_stables/gale_shapley.ml
+++ _build/default/src/mariages_stables/gale_shapley.ml
@@ -30,7 +30,7 @@ let algo ?(affiche_config=true) entree =
     (* initialisation tableau de prétendants *)
     let pretendants = ref (Array.make 4 []) in
 
-    
+    let cpt = ref 0 in
 
     (* fiancer toutes les femmes à Ω; *)
     let config = {
@@ -38,8 +38,14 @@ let algo ?(affiche_config=true) entree =
       fiance_de = Array.make n omega;
     } in
 
+    let rec print_list = function 
+      |[] -> ()
+      | e::l -> print_int e ; print_string " " ; print_list l ; print_string "\n" in
+
 
     while List.length !hommes_celibataire <> 0 do
+
+        cpt := !cpt +  1;
   
         let homme_courant = ref 0 in
         let next_women = ref 0 in
@@ -51,21 +57,17 @@ let algo ?(affiche_config=true) entree =
         next_women := config.rang_appel_de.(!homme_courant); (* <- femme suivante dans le rang d'appel *)
         index_women := entree.liste_appel_de.(!homme_courant).(!next_women); (* <- index de la liste de preference d'un homme *)
         
-        ignore index_women;
-        print_int !index_women;
-        print_string "\n";
-        
         !pretendants.(!index_women) <- !pretendants.(!index_women) @ [!homme_courant]
       done;
 
       (* pour toute femme x ayant reçu une proposition *)
 
+      let rang_a_avancer = ref [] in
+
       for i=0 to n-1 do
         let current_list = ref (!pretendants.(i)) in
         if List.length !current_list <> 0
-
         then begin
-
           (* X := prétendant que x préfère *)
 
           let rec meilleur_pretendant l r k = match l with (* l = current liste, r =  *)
@@ -83,7 +85,6 @@ let algo ?(affiche_config=true) entree =
 
             let meilleur_pretendant_actuel = ref (List.hd !current_list) in
             x := Some (meilleur_pretendant !current_list meilleur_pretendant_actuel i); (* <- voici X, le prétendant que x préfère *)
-
             (*
             si x préfère X à son fiancé actuel, ou si x est célibataire, faire début
             fiancer x et X (X n'est plus célibataire, l'éventuel ancien fiançé de x le devient)
@@ -91,13 +92,15 @@ let algo ?(affiche_config=true) entree =
             fin
             *)
 
-
+            
             let actuel_fiance_de__x = config.fiance_de.(i) in
 
             if actuel_fiance_de__x = None
               then begin
+                
                 config.fiance_de.(i) <- !x;
                 hommes_celibataire := List.filter (fun a -> a <> (Option.get !x)) !hommes_celibataire (* retirer X de la liste des célibataires *)
+                
               end
             else begin
               if entree.prefere.(i) (Option.get !x) (Option.get actuel_fiance_de__x)
@@ -105,53 +108,46 @@ let algo ?(affiche_config=true) entree =
                   config.fiance_de.(i) <- !x;
                   next_hommes_celibataire := List.filter (fun a -> a <> (Option.get !x)) !next_hommes_celibataire; (* retirer X de la liste des célibataires *)
                   next_hommes_celibataire := Option.get actuel_fiance_de__x::!next_hommes_celibataire;
-                  let tmp = config.rang_appel_de.(Option.get (actuel_fiance_de__x)) in
-                  config.rang_appel_de.(Option.get (actuel_fiance_de__x)) <- tmp + 1;
-                end;
-              
-            end;
-            
+                  
+                  if not (List.mem (Option.get actuel_fiance_de__x) !rang_a_avancer)
+                  then begin
+                    rang_a_avancer := (Option.get actuel_fiance_de__x)::!rang_a_avancer
+                  end
 
-            (*
-            pour tout prétendant Y de x différent de son fiancé actuel,
-            avancer le rang d'appel de Y (et laisser Y parmi les célibataires)
-            remettre à ø l'ensemble des prétendants de x
-            *)
-
-            if config.fiance_de.(i) = !x (* Cas 1 : x est devenu l'actuel fiancé donc il reste tous les autres prétendants *)
-              then begin
-                current_list := List.filter (fun a -> a <> (Option.get !x)) !current_list;
-                for e=0 to (List.length !current_list)-1 do
-                  config.rang_appel_de.(e) <- config.rang_appel_de.(e) + 1;
-                done;
-                !pretendants.(i) <- []
-              end
-
-            else begin  (* Cas 2 : x n'est pas devenu l'actuel fiancé *)
-                for e=0 to (List.length !current_list)-1 do
-                  config.rang_appel_de.(e) <- config.rang_appel_de.(e) + 1;
-                done;
-                !pretendants.(i) <- []
-            end;
-
-              !pretendants.(i) <- []
-
+                end
+              else begin
+                if not (List.mem (Option.get !x) !rang_a_avancer)
+                  then begin
+                    rang_a_avancer := (Option.get !x)::!rang_a_avancer
+                  end
+              end;
+            end;        
         end;
-    done;
+      done;
 
-    if affiche_config = true
-    then begin
-      print_configuration config
-    end;
-
-    hommes_celibataire := !next_hommes_celibataire;
-    ignore (exit 0);
+      if (List.length !rang_a_avancer) <> 0 && !cpt > 0
+      then begin
+        for i=0 to (List.length !rang_a_avancer)-1 do
+          let a_avancer = List.nth !rang_a_avancer i in
+          if (List.length !next_hommes_celibataire) <> 0 then begin
+            config.rang_appel_de.(a_avancer) <- config.rang_appel_de.(a_avancer) + 1;
+          end;
+        done;
+      end;
+      
+      ignore print_list;
+      if affiche_config = true
+        then begin
+          print_configuration config
+        end;
     
-  done;
+        hommes_celibataire := !next_hommes_celibataire;
+      
+      
 
-  
-  (*célébrer n mariages*)
-  for i=0 to n-1 do
+    done;
+     (*célébrer n mariages*)
+    for i=0 to n-1 do
       res := (Option.get config.fiance_de.(i),i)::!res
     done;
     !res
